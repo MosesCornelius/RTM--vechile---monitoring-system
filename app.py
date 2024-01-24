be88da29ffe9bf660f21faa1e137d817c578db33
MosesCornelius <93873055+MosesCornelius@users.noreply.github.com>
2024-01-24 06:22:53 +0000

    added flask to the project

---
+++ app.py
@@ -0,0 +1,13 @@
+from re import DEBUG
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def Hello_NANCY():
+  return 'Hello, NANCY!'
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
