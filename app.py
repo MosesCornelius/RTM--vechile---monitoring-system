4ee27cf8dd7309c55593503283dd74eb42ed5c04
MosesCornelius <93873055+MosesCornelius@users.noreply.github.com>
2024-02-06 03:57:13 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from re import DEBUG
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def Hello_NANCY():
-  return 'Hello, NANCY!'
-
+@app.route("/")
+def hello_RTM():
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
