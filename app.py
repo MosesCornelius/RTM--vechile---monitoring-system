6b5dca5fd723169a1ab4a837a6fe5271ce1a8a88
MosesCornelius <93873055+MosesCornelius@users.noreply.github.com>
2024-02-06 16:24:02 +0000

    new update

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_RTM():
-  return render_template('home.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
